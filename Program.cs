28e85444aef512e71c9c514e979cd23695b591ca
nathan-marrero-petruci <nathan_marrero@hotmail.com>
2025-08-04 14:23:52 -0300

    chore: adiciona logging de console detalhado para debug em produção

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ using ToDoList.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Logging detalhado para produção/deploy
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
 // Railway exige que o app rode na porta 8080
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
